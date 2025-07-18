a2e90e68a3695bf4b06d78efadc9b619f458939e
wagura Maurice <buisness@waguramaurice.com>
2025-07-18 03:12:54 +0300

    update ITodoRepository.cs

--- Interfaces/ITodoRepository.cs
+++ Interfaces/ITodoRepository.cs
@@ -6,7 +6,8 @@ namespace TodoApp.Interfaces
 {
     public interface ITodoRepository
     {
-        Task<IEnumerable<TodoItem>> GetUserTodosAsync(string userId, bool? isCompleted = null);
+        Task<IEnumerable<TodoItem>> GetUserTodosAsync(string userId, bool? isCompleted = null, int page = 1, int pageSize = 10);
+        Task<int> GetUserTodosCountAsync(string userId, bool? isCompleted = null);
         Task<TodoItem?> GetTodoByIdAsync(int id, string userId);
         Task AddTodoAsync(TodoItem todo);
         Task UpdateTodoAsync(TodoItem todo);

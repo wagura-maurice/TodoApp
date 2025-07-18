0bdf65ab7f47eb54034d99a9337ee011a4501943
wagura Maurice <buisness@waguramaurice.com>
2025-07-18 03:13:03 +0300

    update EmailSender.cs

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,10 +1,20 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace TodoApp.Services
 {
-    public class EmailSender : IEmailSender
+    public class EmailSender : IEmailSender<IdentityUser>
     {
+        public Task SendConfirmationLinkAsync(IdentityUser user, string email, string confirmationLink) => 
+            SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+
+        public Task SendPasswordResetLinkAsync(IdentityUser user, string email, string resetLink) =>
+            SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+
+        public Task SendPasswordResetCodeAsync(IdentityUser user, string email, string resetCode) =>
+            SendEmailAsync(email, "Reset your password", $"Your password reset code is: {resetCode}");
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             // In a real application, this would send an email
